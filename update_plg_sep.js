8add1cc52265b7ae7a2b10436ba148857de5e888
megonodev <megonokomuniti@gmail.com>
2018-07-25 05:11:27 -0700

    update apidoc

---
+++ update_plg_sep.js
@@ -0,0 +1,46 @@
+var currentUser = {
+    name: 'Sugandi'
+};
+
+/**
+ * @api {put} /sep/updtglplg updtglplg
+ * @apiName updtglplg
+ * @apiGroup SEP
+ * 
+ * @apiParam {json} json request json 
+ * 
+ * @apiExample Example usage:
+ * curl --request PUT \
+ *      --url 'https://api.rsudkraton.com/sep/updtglplg' \
+ *      --Content-Type 'application/json' \
+ * 
+ * @apiSuccess {json} metadata Informasi code dan pesan
+ * @apiSuccess {json} response Informasi SEP
+ * 
+ * @apiSuccessExample {json} Request-Sample:
+ *                                                     
+ *                                                  
+ *   {
+ *      "request": {
+ *         "t_sep": 
+ *          {
+ *              "noSep":"0301R00105160000569",
+ *              "tglPulang":"2016-06-12",
+ *              "user":"Coba Ws"
+ *          }
+ *      }
+ *   }                
+ * 
+ * @apiSuccessExample {json} Success-Response:
+ * 
+ *     {
+ *        "metaData": {
+ *            "code": "200",
+ *            "message": "Ok"
+ *         },
+ *         "response": "0301R00105160000569"
+ *      }
+ */
+function updateplg() {
+    return; 
+}
